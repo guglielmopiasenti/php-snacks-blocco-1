7547793f04303895016a636bd1f6cfbee943aa31
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-17 13:55:04 +0200

    title + style

--- snack1/Index.php
+++ snack1/Index.php
@@ -58,10 +58,11 @@ $partite = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title></title>
+    <title>Campionato Serie A</title>
 </head>
 <body>
 <div class="container">
+    <h1>Campionato Serie A Basket</h1>
         <?php foreach ($partite as $partita): ?>
             <p>
                 <?= $partita['squadra_casa'] ?> - <?= $partita['squadra_ospite'] ?> | <?= $partita['punti_casa'] ?>-<?= $partita['punti_ospite'] ?>
