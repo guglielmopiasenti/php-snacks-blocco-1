c097a0f6804b7bf15c51ae17aab9972c31db1177
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-17 14:26:50 +0200

    inserting into HTML

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -1,15 +1,33 @@
-<?php
-if (isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {
-    $name = $_GET['name'];
-    $mail = $_GET['mail'];
-    $age = $_GET['age'];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autenticathor</title>
+</head>
+<body>
+    <div class="container">
+    <?php
+    if (isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {
+        $name = $_GET['name'];
+        $mail = $_GET['mail'];
+        $age = $_GET['age'];
 
-    if (strlen($name) > 3 && strpos($mail, '.') !== false && strpos($mail, '@') !== false && is_numeric($age)) {
-        echo "Accesso riuscito";
+        if (strlen($name) > 3 && strpos($mail, '.') !== false && strpos($mail, '@') !== false && is_numeric($age)) {
+    ?>
+        <p>Accesso riuscito</p>
+    <?php
+        } else {
+    ?>
+        <p>Accesso negato</p>
+    <?php
+        }
     } else {
-        echo "Accesso negato";
+    ?>
+        <p>Accesso negato</p>
+    <?php
     }
-} else {
-    echo "Accesso negato";
-}
-?>
+    ?>
+    </div>
+</body>
+</html>
