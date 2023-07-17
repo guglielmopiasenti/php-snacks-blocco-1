6b6853b2eaa4e8e00be57ecc6c65f61ab4af9191
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-17 13:17:55 +0200

    adding array

--- snack1/Index.php
+++ snack1/Index.php
@@ -1,1 +1,54 @@
+<?php
+
+$partite = [
+    [
+        "squadra_casa" => "Olimpia Milano",
+        "squadra_ospite" => "Banco di Sardegna Sassari",
+        "punti_casa" => rand(50, 100),
+        "punti_ospite" => rand(50, 100),
+    ],
+    [
+        "squadra_casa" => "Virtus Segafredo Bologna",
+        "squadra_ospite" => "A|X Armani Exchange Milano",
+        "punti_casa" => rand(50, 100),
+        "punti_ospite" => rand(50, 100),
+    ],
+    [
+        "squadra_casa" => "Dolomiti Energia Trentino",
+        "squadra_ospite" => "Openjobmetis Varese",
+        "punti_casa" => rand(50, 100),
+        "punti_ospite" => rand(50, 100),
+    ],
+    [
+        "squadra_casa" => "Allianz Pallacanestro Trieste",
+        "squadra_ospite" => "Happy Casa Brindisi",
+        "punti_casa" => rand(50, 100),
+        "punti_ospite" => rand(50, 100),
+    ],
+    [
+        "squadra_casa" => "Carpegna Prosciutto Basket Pesaro",
+        "squadra_ospite" => "Fratelli Beretta Derthona",
+        "punti_casa" => rand(50, 100),
+        "punti_ospite" => rand(50, 100),
+    ],
+    [
+        "squadra_casa" => "Germani Brescia Leonessa",
+        "squadra_ospite" => "De Longhi Treviso",
+        "punti_casa" => rand(50, 100),
+        "punti_ospite" => rand(50, 100),
+    ],
+    [
+        "squadra_casa" => "Vanoli Cremona",
+        "squadra_ospite" => "Umana Reyer Venezia",
+        "punti_casa" => rand(50, 100),
+        "punti_ospite" => rand(50, 100),
+    ],
+    [
+        "squadra_casa" => "UNAHOTELS Reggio Emilia",
+        "squadra_ospite" => "Pallacanestro Varese",
+        "punti_casa" => rand(50, 100),
+        "punti_ospite" => rand(50, 100),
+    ],
+];
+?>
 
